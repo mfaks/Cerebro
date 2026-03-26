8d1db1e5f36c5bf168f6a340320d2cbe53f70e5e
mfaks <muhammad.faks@gmail.com>
2026-03-25 17:57:02 -0700

    feat: installed inital arcgis configuration

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,24 @@
+import { useEffect, useRef } from 'react'
+import Map from '@arcgis/core/Map.js'
+import MapView from '@arcgis/core/views/MapView.js'
+
 function App() {
-  return <></>
+  const mapRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    const map = new Map({ basemap: 'dark-gray-vector' })
+    const view = new MapView({
+      container: mapRef.current!,
+      map,
+      center: [0, 0],
+      zoom: 2,
+    })
+    return () => {
+      view.destroy()
+    }
+  }, [])
+
+  return <div ref={mapRef} style={{ width: '100vw', height: '100vh' }} />
 }
 
 export default App
